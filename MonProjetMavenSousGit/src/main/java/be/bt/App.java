83c53f1c21cae97cb70c14db8a9029096bb63aab
student <student@PARIS3-6.bt-training.be>
2017-09-04 12:01:51 +0200

    ajout de slf4j

--- MonProjetMavenSousGit/src/main/java/be/bt/App.java
+++ MonProjetMavenSousGit/src/main/java/be/bt/App.java
@@ -15,5 +15,6 @@ public class App
 	private static void showLoggingFrameworks() {
 		// TODO Auto-generated method stub
 		System.out.println("======= LOG4J ===============");
+		System.out.println("======= SLF4J Facade ===============");
 	}
 }
