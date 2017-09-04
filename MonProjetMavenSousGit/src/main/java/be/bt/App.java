732e91b403aa1607f4f160ca3250b19a54785ef6
student <student@PARIS3-6.bt-training.be>
2017-09-04 12:01:19 +0200

    log4j

--- MonProjetMavenSousGit/src/main/java/be/bt/App.java
+++ MonProjetMavenSousGit/src/main/java/be/bt/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        showLoggingFrameworks();
     }
+
+	private static void showLoggingFrameworks() {
+		// TODO Auto-generated method stub
+		System.out.println("======= LOG4J ===============");
+	}
 }
